82a5e4764cf5c5f044f5c8e608be901ec651e2c8
goodmn <goodmn79@gmail.com>
2024-12-06 16:00:50 +0300

    setup

---
+++ src/test/java/com/gdmn/ugarden/UgardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdmn.ugarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UgardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
